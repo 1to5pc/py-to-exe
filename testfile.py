2ec6bfef3f5f12bbcf63823faeeafb866b0526e2
1to5PC <80444139+1to5pc@users.noreply.github.com>
2021-08-31 07:14:33 +0530

    Add files via upload

---
+++ testfile.py
@@ -0,0 +1,7 @@
+import time
+print("Hello World")
+print("type in your name below:")
+name = input("hello type here:" )
+print("Hello", name)
+print("nice to meet you")
+time.sleep(2) # Sets sleep for 2 sec
